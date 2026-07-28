5937e8828857c367ef39628660a36ad170c88f4f
BLOOD-MD <nunu001936@gmail.com>
2026-07-28 19:23:02 +0500

    Add file upload middleware to createBuild route

--- src/routes/build.routes.js
+++ src/routes/build.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { createBuild } from "../controllers/build.controller.js";
+import upload from "../middlewares/upload.middleware.js";
 
 const router = express.Router();
 
-router.post("/create", createBuild);
+router.post(
+  "/create",
+  upload.single("project"),
+  createBuild
+);
 
 export default router;
